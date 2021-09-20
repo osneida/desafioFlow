840038521d0bab1697862dab2a71850387ab9438
osneidabordones <osneidabordones@gmail.com>
2021-09-20 01:36:01 -0400

    inicializado typescript

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+//# sourceMappingURL=app.js.map
+
+Object.defineProperty(exports,"__esModule", { value: true });
+exports.nombre = void 0;
+exports.nombre ='Osneida';
+
+console.log(exports.nombre);
